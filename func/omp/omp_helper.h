532927e37fba193d50d0f686ea54ad0700d8fa52
Mayeul Fournial <mfournial@outlook.com>
2020-02-03 12:22:27 +0000

    Fix deleted empty constructor

--- func/omp/omp_helper.h
+++ func/omp/omp_helper.h
@@ -3,6 +3,7 @@
 
 struct AlignedElem {
     int32_t i = 0;
+    AlignedElem() {};
     AlignedElem(int32_t i) : i(i) {};
     friend inline bool operator==(const AlignedElem& lhs, const AlignedElem& rhs) {
         return lhs.i == rhs.i;
